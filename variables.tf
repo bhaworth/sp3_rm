754c46524819eed4f062782d3c378989b426a0d3
bhaworth <ben@benhaworth.com>
2021-03-12 07:29:20 +0000

    First draft schema.yaml

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "tenancy_ocid" {}
+variable "region" {}
+variable "targetCompartment" {}
+variable "ad" {}
+variable "ssh_pub_key" {}
+variable "bastion_shape" {}
+variable "headnode_shape" {}
